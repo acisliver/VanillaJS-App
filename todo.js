d27efe64f7cf65ccbdaac4fb7f2b30f470cac666
acisliver <7273322@naver.com>
2019-10-16 15:02:28 +0900

    toDoList

--- todo.js
+++ todo.js
@@ -2,17 +2,31 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("Input"),
   toDoList = document.querySelector(".js-toDoList");
 
-const TODOS_LS = 'toDos'
+const TODOS_LS = 'toDos';
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
 
 function paintTodo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
-  delBtn.innerHTML = "x"
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "❌"
   span.innerText = text;
   li.appendChild(span);     //li안에 span추가
   li.appendChild(delBtn);   //li안에 dlebtn추가
+  li.id = newId;
   toDoList.appendChild(li); //toDoList(ul)안에 li(위에 추가된 내용까지)추가
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj)
+  saveToDos();
 }
 
 function handleSubmit(event) {
@@ -21,16 +35,17 @@ function handleSubmit(event) {
   paintTodo(currentValue);
   toDoInput.value = "";
 
-function handleClick(event) {
 
-}
 
 }
 
 function loadToDo() {
-  const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos == null){
-
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo) {
+          console.log(toDo.text);
+        });
     }
 }
 
